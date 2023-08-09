87a7922f5b58f522ca7a3ba0bd37662fd4952c8b
Felipe Barbedo <felipebarbedo54@gmail.com>
2023-08-09 10:13:19 -0300

    Capture update

--- checkers.c
+++ checkers.c
@@ -1,3 +1,7 @@
+// cd /mnt/c/Users/felip/Desktop/Study-WS/IPPD/Checkers_Game
+// gcc -o checkers checkers.c -Wall
+// ./checkers
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -12,223 +16,31 @@ typedef struct
 	int target_col;
 } Move;
 
-void free_valid_moves(Move* valid_moves)
-{
-	free(valid_moves);
-}
+int** create_board();
 
-void print_board(int** board)
-{
-	printf("   ");
-	for (int i = 0; i < BOARD_SIZE; i++)
-		printf("%d ", i);
+void free_board(int** board);
 
-	printf("\n   ");
-	for (int i = 0; i < 15; i++)
-		printf("_");
+void print_board(int** board);
 
-	printf("\n");
-	for (int i = 0; i < BOARD_SIZE; i++)
-	{
-		//printf("%c |", ('A' + i));
-		printf("%d |", i);
+int is_inside_board(int row, int col);
 
-		for (int j = 0; j < BOARD_SIZE; j++)
-			printf("%d ", board[i][j]);
+int is_empty_cell(int** board, int row, int col);
 
-		printf("\n");
-	}
-}
+int is_player_piece(int** board, int row, int col, int player);
 
-int is_inside_board(int row, int col)
-{
-	return (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE);
-}
+int is_capture_position(int** board, int row, int col, int player);
 
-int is_empty_cell(int** board, int row, int col) 
-{
-	return (board[row][col] == 0);
-}
+void add_valid_move(Move* valid_moves, int* num_moves, int source_row, int source_col, int target_row, int target_col);
 
-int is_player_piece(int** board, int row, int col, int player)
-{
-	return (board[row][col] == player);
-}
+void get_valid_moves_for_piece(int** board, int row, int col, int player, Move* valid_moves, int* num_moves);
 
-int is_capture_position(int** board, int row, int col, int player)
-{
-	if (!is_inside_board(row, col) || !is_empty_cell(board, row, col))
-	{
-		return 0;
-	}
+void get_valid_capture_for_piece(int** board, int row, int col, int player, Move* valid_moves, int* num_moves);
 
-	if (player == 1)
-	{
-		if (is_empty_cell(board, row + 2, col - 2) && 
-			is_player_piece(board, row + 1, col - 1, player))
-		{
-			return 1;
-		}
+Move* get_valid_moves(int** board, int player, int* num_moves);
 
-		if (is_empty_cell(board, row + 2, col + 2) &&
-			is_player_piece(board, row + 1, col + 1, player))
-		{
-			return 1;
-		}
-	}
+void free_valid_moves(Move* valid_moves);
 
-	if (player == 2)
-	{
-		if (is_empty_cell(board, row - 2, col - 2) &&
-			is_player_piece(board, row - 1, col - 1, player))
-		{
-			return 1;
-		}
-
-		if (is_empty_cell(board, row - 2, col + 2) &&
-			is_player_piece(board, row - 1, col + 1, player))
-		{
-			return 1;
-		}
-	}
-	
-	return 0;
-}
-
-void add_valid_move(Move* valid_moves, int* num_moves, int source_row, int source_col, int target_row, int target_col)
-{
-	valid_moves[*num_moves].source_row = source_row;
-	valid_moves[*num_moves].source_col = source_col;
-	valid_moves[*num_moves].target_row = target_row;
-	valid_moves[*num_moves].target_col = target_col;
-	(*num_moves)++;
-}
-
-void get_valid_moves_for_piece(int** board, int row, int col, int player, Move* valid_moves, int* num_moves)
-{
-	if (player == 1)
-	{
-		// Left Move P1
-		if (is_inside_board(row + 1, col - 1) && is_empty_cell(board, row + 1, col - 1))
-			add_valid_move(valid_moves, num_moves, row, col, row + 1, col - 1);
-
-		// Right Move P1
-		if (is_inside_board(row + 1, col + 1) && is_empty_cell(board, row + 1, col + 1))
-			add_valid_move(valid_moves, num_moves, row, col, row + 1, col + 1);
-
-		// Left Capture P1
-		if (is_capture_position(board, row + 1, col - 1, player))
-			add_valid_move(valid_moves, num_moves, row, col, row + 2, col - 2);
-
-		// Right Capture P1
-		if (is_capture_position(board, row + 1, col + 1, player))
-			add_valid_move(valid_moves, num_moves, row, col, row + 2, col + 2);
-
-	} else {
-		// Left Move P2
-		if (is_inside_board(row - 1, col - 1) && is_empty_cell(board, row - 1, col - 1))
-			add_valid_move(valid_moves, num_moves, row, col, row - 1, col - 1);
-
-		// Right Move P2
-		if (is_inside_board(row - 1, col + 1) && is_empty_cell(board, row - 1, col + 1))
-			add_valid_move(valid_moves, num_moves, row, col, row - 1, col + 1);
-
-		// Left Capture P1
-		if (is_capture_position(board, row - 1, col - 1, player))
-			add_valid_move(valid_moves, num_moves, row, col, row - 2, col - 2);
-
-		// Right Capture P1
-		if (is_capture_position(board, row - 1, col + 1, player))
-			add_valid_move(valid_moves, num_moves, row, col, row - 2, col + 2);
-
-	}
-}
-
-Move* get_valid_moves(int** board, int player, int* num_moves)
-{
-	Move* valid_moves = (Move*)malloc(BOARD_SIZE * BOARD_SIZE * sizeof(Move));
-	*num_moves = 0;
-
-	for (int row = 0; row < BOARD_SIZE; row++)
-		for (int col = 0; col < BOARD_SIZE; col++)
-		{
-			if (is_player_piece(board, row, col, player))
-			{
-				get_valid_moves_for_piece(board, row, col, player, valid_moves, num_moves);
-			}
-		}
-
-	return valid_moves;
-}
-
-void make_move(int** board, Move move, Move* valid_moves, int num_moves)
-{
-	int player = board[move.source_row][move.source_col];
-
-	if (is_capture_position(board, move.target_row, move.target_col, player))
-	{
-		for (int i = 0; i < num_moves; i++)
-		{
-			if (move.source_row == valid_moves[i].source_row && 
-				move.source_col == valid_moves[i].source_col)
-			{
-				if (move.target_row == valid_moves[i].target_row && 
-					move.target_col == valid_moves[i].target_col)
-				{
-					board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-					if (player == 1)
-					{
-						if (move.target_row > 0 && move.target_col < 0) 
-						{
-							board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-							board[move.source_row][move.source_col] = 0;
-							board[move.source_row + 1][move.source_col - 1] = 0;
-						}
-
-						if (move.target_row > 0 && move.target_col > 0) 
-						{
-							board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-							board[move.source_row][move.source_col] = 0;
-							board[move.source_row + 1][move.source_col + 1] = 0;
-						}
-					}
-
-					if (player == 2)
-					{
-						if (move.target_row < 0 && move.target_col < 0) 
-						{
-							board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-							board[move.source_row][move.source_col] = 0;
-							board[move.source_row - 1][move.source_col - 1] = 0;
-						}
-
-						if (move.target_row < 0 && move.target_col > 0) 
-						{
-							board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-							board[move.source_row][move.source_col] = 0;
-							board[move.source_row - 1][move.source_col + 1] = 0;
-						}
-					}
-					
-				}
-			}
-		}
-	} else {
-		for (int i = 0; i < num_moves; i++)
-		{
-			if (move.source_row == valid_moves[i].source_row && 
-				move.source_col == valid_moves[i].source_col)
-			{
-				if (move.target_row == valid_moves[i].target_row && 
-				move.target_col == valid_moves[i].target_col)
-				{
-					board[move.target_row][move.target_col] = board[move.source_row][move.source_col];
-					board[move.source_row][move.source_col] = 0;
-				}
-			}
-		}	
-	}
-}
+void make_move(int** board, Move move, Move* valid_moves, int num_moves);
 
 void undo_move(int** board, Move move)
 {
@@ -393,6 +205,68 @@ void robot_move(int** board)
 	free_valid_moves(valid_moves);
 }
 
+int main()
+{
+	int** board = create_board();
+	int player;
+
+	int num_moves;
+	Move move;
+	Move* valid_moves;
+
+	player = 1;
+	print_board(board);
+
+	valid_moves = get_valid_moves(board, player, &num_moves);
+	move.source_row = 2;
+	move.source_col = 1;
+	move.target_row = 3;
+	move.target_col = 2;
+	make_move(board, move, valid_moves, num_moves);
+
+	player = 2;
+	print_board(board);
+
+	valid_moves = get_valid_moves(board, player, &num_moves);
+	move.source_row = 5;
+	move.source_col = 4;
+	move.target_row = 4;
+	move.target_col = 3;
+	make_move(board, move, valid_moves, num_moves);
+
+
+	player = 1;
+	print_board(board);
+
+	valid_moves = get_valid_moves(board, player, &num_moves);
+	move.source_row = 3;
+	move.source_col = 2;
+	move.target_row = 5;
+	move.target_col = 4;
+	make_move(board, move, valid_moves, num_moves);
+
+	player = 2;
+	print_board(board);
+
+	valid_moves = get_valid_moves(board, player, &num_moves);
+/*
+	make_move(board, move, valid_moves, num_moves);
+
+	print_board(board);
+*/
+	for (int i = 0; i < num_moves; i++)
+	{
+		printf("Move %d: (%d, %d) -> (%d, %d)\n", i + 1, valid_moves[i].source_row,
+			valid_moves[i].source_col, valid_moves[i].target_row, valid_moves[i].target_col);
+	}
+
+	free_valid_moves(valid_moves);
+
+	free_board(board);
+
+	return 0;
+}
+
 int** create_board()
 {
 	int** board = (int**)malloc(BOARD_SIZE * sizeof(int*));
@@ -441,41 +315,227 @@ void free_board(int** board)
 	free(board);
 }
 
-int main()
+void print_board(int** board)
 {
-	int** board = create_board();
-	int player = 1;
-	int num_moves;
+	printf("   ");
+	for (int i = 0; i < BOARD_SIZE; i++)
+		printf("%d ", i);
 
-	Move* valid_moves;
-	Move move;
+	printf("\n   ");
+	for (int i = 0; i < 15; i++)
+		printf("_");
 
-	valid_moves = get_valid_moves(board, player, &num_moves);
-		
-	move.source_row = 2;
-	move.source_col = 1;
-	move.target_row = 3;
-	move.target_col = 0;
-
-	print_board(board); //1
-
-	make_move(board, move, valid_moves, num_moves);
-
-	valid_moves = get_valid_moves(board, player, &num_moves);
-
-	robot_move(board);
-
-	print_board(board);
-
-	free_valid_moves(valid_moves);
-
-	free_board(board);
-
-	/*for (int i = 0; i < num_moves; i++)
+	printf("\n");
+	for (int i = 0; i < BOARD_SIZE; i++)
 	{
-		printf("Move %d: (%d, %d) -> (%d, %d)\n", i + 1, valid_moves[i].source_row,
-			valid_moves[i].source_col, valid_moves[i].target_row, valid_moves[i].target_col);
-	}*/
+		//printf("%c |", ('A' + i));
+		printf("%d |", i);
 
+		for (int j = 0; j < BOARD_SIZE; j++)
+			printf("%d ", board[i][j]);
+
+		printf("\n");
+	}
+}
+
+int is_inside_board(int row, int col)
+{
+	return (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE);
+}
+
+int is_empty_cell(int** board, int row, int col) 
+{
+	return (board[row][col] == 0);
+}
+
+int is_player_piece(int** board, int row, int col, int player)
+{
+	return (board[row][col] == player);
+}
+
+int is_capture_position(int** board, int row, int col, int player)
+{
+	if (player == 1)
+	{
+		if (is_inside_board(row + 2, col - 2) && is_empty_cell(board, row + 2, col - 2) && 
+			!is_player_piece(board, row + 1, col - 1, player) && 
+			!is_empty_cell(board, row + 1, col - 1))
+		{
+			return 1;
+		}
+
+		if (is_inside_board(row + 2, col + 2) && is_empty_cell(board, row + 2, col + 2) &&
+			!is_player_piece(board, row + 1, col + 1, player) && 
+			!is_empty_cell(board, row + 1, col + 1))
+		{
+			return 2;
+		}
+	}
+
+	if (player == 2)
+	{
+		if (is_inside_board(row - 2, col - 2) && is_empty_cell(board, row - 2, col - 2) &&
+			!is_player_piece(board, row - 1, col - 1, player) && 
+			!is_empty_cell(board, row - 1, col - 1))
+		{
+			return 3;
+		}
+
+		if (is_inside_board(row - 2, col + 2) && is_empty_cell(board, row - 2, col + 2) &&
+			!is_player_piece(board, row - 1, col + 1, player) && 
+			!is_empty_cell(board, row - 1, col + 1))
+		{
+			return 4;
+		}
+	}
+	
 	return 0;
+}
+
+void add_valid_move(Move* valid_moves, int* num_moves, int source_row, int source_col, int target_row, int target_col)
+{
+	valid_moves[*num_moves].source_row = source_row;
+	valid_moves[*num_moves].source_col = source_col;
+	valid_moves[*num_moves].target_row = target_row;
+	valid_moves[*num_moves].target_col = target_col;
+	(*num_moves)++;
+}
+
+void get_valid_moves_for_piece(int** board, int row, int col, int player, Move* valid_moves, int* num_moves)
+{
+	if (player == 1)
+	{
+		// Left Move P1
+		if (is_inside_board(row + 1, col - 1) && is_empty_cell(board, row + 1, col - 1))
+			add_valid_move(valid_moves, num_moves, row, col, row + 1, col - 1);
+
+		// Right Move P1
+		if (is_inside_board(row + 1, col + 1) && is_empty_cell(board, row + 1, col + 1))
+			add_valid_move(valid_moves, num_moves, row, col, row + 1, col + 1);
+
+	} else {
+		// Left Move P2
+		if (is_inside_board(row - 1, col - 1) && is_empty_cell(board, row - 1, col - 1))
+			add_valid_move(valid_moves, num_moves, row, col, row - 1, col - 1);
+
+		// Right Move P2
+		if (is_inside_board(row - 1, col + 1) && is_empty_cell(board, row - 1, col + 1))
+			add_valid_move(valid_moves, num_moves, row, col, row - 1, col + 1);
+
+	}
+}
+
+void get_valid_capture_for_piece(int** board, int row, int col, int player, Move* valid_moves, int* num_moves)
+{
+	int aux = is_capture_position(board, row, col, player);
+
+	switch (aux)
+	{
+	case 0:
+		break;
+
+	case 1:
+		add_valid_move(valid_moves, num_moves, row, col, row + 2, col - 2);
+		break;
+
+	case 2:
+		add_valid_move(valid_moves, num_moves, row, col, row + 2, col + 2);
+		break;
+
+	case 3:
+		add_valid_move(valid_moves, num_moves, row, col, row - 2, col - 2);
+		break;
+
+	case 4:
+		add_valid_move(valid_moves, num_moves, row, col, row - 2, col + 2);
+		break;
+	}
+}
+
+Move* get_valid_moves(int** board, int player, int* num_moves)
+{
+	Move* valid_moves = (Move*)malloc(BOARD_SIZE * BOARD_SIZE * sizeof(Move));
+	*num_moves = 0;
+
+	for (int row = 0; row < BOARD_SIZE; row++)
+		for (int col = 0; col < BOARD_SIZE; col++)
+		{
+			if (is_player_piece(board, row, col, player))
+			{
+				get_valid_moves_for_piece(board, row, col, player, valid_moves, num_moves);
+				get_valid_capture_for_piece(board, row, col, player, valid_moves, num_moves);
+			}
+		}
+
+	return valid_moves;
+}
+
+void free_valid_moves(Move* valid_moves)
+{
+	free(valid_moves);
+}
+
+void make_move(int** board, Move move, Move* valid_moves, int num_moves)
+{
+	int player = board[move.source_row][move.source_col];
+
+	if (is_capture_position(board, move.source_row, move.source_col, player))
+	{
+		for (int i = 0; i < num_moves; i++)
+		{
+			if (move.source_row == valid_moves[i].source_row && 
+				move.source_col == valid_moves[i].source_col)
+			{
+				if (move.target_row == valid_moves[i].target_row && 
+					move.target_col == valid_moves[i].target_col)
+				{
+					if (player == 1)
+					{
+						board[move.target_row][move.target_col] = 
+						board[move.source_row][move.source_col];
+
+						board[move.source_row][move.source_col] = 0;
+
+						if ((move.target_col - move.source_col) < 0) 
+						{
+							board[move.source_row + 1][move.source_col - 1] = 0;
+						} else {
+							board[move.source_row + 1][move.source_col + 1] = 0;
+						}
+					}
+
+					if (player == 2)
+					{
+						board[move.target_row][move.target_col] = 
+						board[move.source_row][move.source_col];
+
+						board[move.source_row][move.source_col] = 0;
+
+						if ((move.target_col - move.source_col) < 0) 
+						{						
+							board[move.source_row - 1][move.source_col - 1] = 0;
+						} else {
+							board[move.source_row - 1][move.source_col + 1] = 0;
+						}
+					}
+				}
+			}
+		}
+	} else {
+		for (int i = 0; i < num_moves; i++)
+		{
+			if (move.source_row == valid_moves[i].source_row && 
+				move.source_col == valid_moves[i].source_col)
+			{
+				if (move.target_row == valid_moves[i].target_row && 
+				move.target_col == valid_moves[i].target_col)
+				{
+					board[move.target_row][move.target_col] = 
+					board[move.source_row][move.source_col];
+
+					board[move.source_row][move.source_col] = 0;
+				}
+			}
+		}	
+	}
 }
